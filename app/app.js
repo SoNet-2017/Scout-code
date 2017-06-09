59253c842ac251060aa5b5d7500eb0026c6e9347
CristianoCap <s244167@studenti.polito.it>
2017-06-09 12:30:46 +0200

    Progetto Base con Angular Material

---
+++ app/app.js
@@ -0,0 +1,94 @@
+/**
+ * Created by criac on 09/06/2017.
+ */
+'use strict';
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDUDFC04BUxoViarP3nA3sdXX2SmZRX-Z4",
+    authDomain: "scrapbook-5b34e.firebaseapp.com",
+    databaseURL: "https://scrapbook-5b34e.firebaseio.com",
+    projectId: "scrapbook-5b34e",
+    storageBucket: "scrapbook-5b34e.appspot.com",
+    messagingSenderId: "806919547565"
+};
+firebase.initializeApp(config);
+
+var app1=angular.module('myApp', [
+        'ngMaterial',
+        'ngRoute',
+        "firebase"
+    ]);
+app1.controller('AppCtrl1', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function(){
+            return $mdSidenav('right').isOpen();
+        };
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+
+        function buildToggler(navID) {
+            return function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            };
+        }
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+
+        };
+    })
+    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav('right').close()
+                .then(function () {
+                    $log.debug("close RIGHT is done");
+                });
+        };
+    });
+
+app1.controller('AppCtrl2', function($scope) {
+
+});
