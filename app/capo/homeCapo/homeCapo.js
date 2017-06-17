ac9c5a1826aad37c13c2f3373ace1f6883857a7d
Simone Montrucchio <simo.montrucchio@gmail.com>
2017-06-17 19:38:08 +0200

    PROSSIMO EVENTO in home capo - funzionante (mostra solo il primo, ma si può usare lo stesso codice per mostrarli tutti)

--- app/capo/homeCapo/homeCapo.js
+++ app/capo/homeCapo/homeCapo.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var app=angular.module('myAppHomeCapo', [
     'ngMaterial',
-    'ngRoute'
+    'ngRoute',
+    'myAppEvento'
 ]);
 
 app.config(['$routeProvider', function($routeProvider){
@@ -17,6 +20,34 @@ app.config(['$routeProvider', function($routeProvider){
     })
 }]);
 
-app.controller('myAppHomeCapoCtrl', function($scope) {
 
-});
+
+app.controller('myAppHomeCapoCtrl', ['$scope','$rootScope', 'Evento', function($scope,$rootScope, Evento) {
+
+    console.log("e' entrato nel ctrl evento");
+
+
+    //initialize variables
+    $scope.dati={};
+    $scope.dati.feedback = "";
+    //get the list of available sq
+    $scope.dati.eventi = Evento.getData();
+    $scope.dati.eventi.$loaded().then(function () {
+
+        for (var i = 0; i < $scope.dati.eventi.length; i++) {
+
+            var uuid = $scope.dati.eventi[i].$id;
+            console.log("Titolo: " + $scope.dati.eventi[i].titolo);
+            console.log("Luogo: " + $scope.dati.eventi[i].luogo);
+            console.log("Data inizio: " + $scope.dati.eventi[i].dataInizio);
+
+            }
+    });
+
+
+
+
+
+
+
+}]);
