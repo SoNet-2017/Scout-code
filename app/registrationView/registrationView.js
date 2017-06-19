710837abfef3587ed96821b9bc270d2fd44cbfe5
@CristianoCap <cristiano.antonio.capanna@gmail.com>
2017-06-19 17:51:06 +0200

    Merge

--- app/registrationView/registrationView.js
+++ app/registrationView/registrationView.js
@@ -5,11 +5,20 @@ var app= angular.module('myAppRegistration', ['ngMaterial', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/registrationView', {
         templateUrl: 'registrationView/registrationView.html',
-        controller: 'registrationViewCtrl'
+        controller: 'myAppRegistrationViewCtrl'
     });
 }]);
 
 
-app.controller('registrationViewCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
+app.controller('myAppRegistrationViewCtrl', ['$scope', '$rootScope', 'Auth', 'Users', '$location', function($scope, $rootScope, Auth, Users, $location) {
     console.log('registration view controller');
+
+    $scope.dati = {};
+
+    $scope.registration = function(){
+        console.log("aggiungiamo il contatto")
+        if($scope.user.password){
+            
+        }
+    }
 }]);
