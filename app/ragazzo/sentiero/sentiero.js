c7e23ff3c94d7b1e91ee8690ee096b3c6624e7c0
@CristianoCap <cristiano.antonio.capanna@gmail.com>
2017-06-28 15:58:50 +0200

    Bisogna sistemare la visualizzazione di Carta Specialita, e la visualizzazione dall'interno di sentiero

--- app/ragazzo/sentiero/sentiero.js
+++ app/ragazzo/sentiero/sentiero.js
@@ -8,24 +8,39 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/sentiero',{
         templateUrl: 'ragazzo/sentiero/sentiero.html',
         controller: 'myAppSentieroCtrl',
-        /* IN ATTESA DI LOGIN
-        resolve: {
+        /*resolve: {
             "currentAuth":["Auth",function(auth) {
                 return Auth.$requireSignIn();
             }]
-        }
-        */
+        }*/
+
     })
 }]);
 
 
-app.controller('myAppSentieroCtrl', function($scope) {
-    console.log("Siamo nel Controller di Sentiero");
-    $scope.specialita = [
-        {
-            "name":"Allevatore",
-            "img":"../"
-        }
-    ]
+app.controller('myAppSentieroCtrl', ['$scope','$rootScope', 'Scadenza', 'Utente', 'CartaSpecialita', 'RegistrazioneCartaSpecialitaService', '$mdDialog', '$firebaseStorage', '$location', function($scope,$rootScope, Scadenza, Utente, CartaSpecialita, RegistrazioneCartaSpecialitaService, $mdDialog, $firebaseStorage, $location){
+    $scope.dati={}
+    $scope.dati.utente=$rootScope.info.user
+    $scope.dati.cartaSpec=CartaSpecialita.getData()
+    $scope.dati.scadenze=Scadenza.getData()
+    $scope.dati.utente=Utente.getData()
+    $scope.dati.cartaSpec.$loaded().then(function(){
+        for(var i = 0; i<$scope.dati.cartaSpec.length; i++){
+            console.log("entro nel for 1")
+            for(var j = 0; j<$scope.dati.scadenze.length; j++){
+                console.log("entro nel for 2")
+                for(var z =0; z<$scope.dati.utente.length; z++) {
+                    if ($scope.dati.scadenze[j].conferma == false && $scope.dati.utente[z].codice == $rootScope.info.user.codice && $scope.dati.scadenze[j].carta_spec == $scope.dati.cartaSpec[i].$id) {
+                        console.log("entro nellif")
+                        $scope.dati.visualizzaCarta = CartaSpecialita.getCartaInfo($scope.dati.cartaSpec[i].$id);
+                        console.log("" + $scope.dati.visualizzaCarta.img_url)
 
-});
+                    }
+                }
+            }
+
+        }
+        /*console.log(""+$scope.dati.visualizzaCarta.img_url)*/
+    })
+
+}]);
