71dfb1f59450648754a4b092192f96dde4ce6fa5
@CristianoCap <cristiano.antonio.capanna@gmail.com>
2017-07-04 19:57:05 +0200

    Ho creato gli aggiornaBio che sono riferiti alla descrizione, agli hooby, all'immagine (anche se, probabilmente non serve e bisogna toglierlo). Ho iniziato a copiare il metodo che serve per il caricamento dell'immagine che bisogna a ssociare ad un ipotetico aggiornaimmagine.

--- app/components/bio/bio-service.js
+++ app/components/bio/bio-service.js
@@ -12,7 +12,33 @@ app.factory('Bio', function($firebaseArray, $firebaseObject) {
         getCartaInfo: function(bioId) {
             var bioRef = firebase.database().ref().child("bio").child(bioId);
             return $firebaseObject(bioRef);
+        },
+        aggiornaBioDescrizione: function(codice, nuovaDescrizione) {
+            var ref = firebase.database().ref().child("bio").child(codice);
+            // create a synchronized array
+            ref.update({
+                descrizione: nuovaDescrizione
+            });
+
+        },
+        aggiornaBioFoto: function(codice, nuovaFoto) {
+            var ref = firebase.database().ref().child("bio").child(codice);
+            // create a synchronized array
+            ref.update({
+                foto_url: nuovaFoto
+            });
+
+        },
+        aggiornaBioHobby: function(codice, nuovoHobby) {
+            var ref = firebase.database().ref().child("bio").child(codice);
+            // create a synchronized array
+            ref.update({
+                hobby: nuovoHobby
+            });
+
         }
+
+
     }
     return bioService;
 })
